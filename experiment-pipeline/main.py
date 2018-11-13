2bdc63616e4cda64e593955844addb52a5b33394
Kirill Shevchenko <kirills167@gmail.com>
2018-11-13 18:29:58 +0200

    normalize

--- experiment-pipeline/main.py
+++ experiment-pipeline/main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 
 names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
 
@@ -27,8 +28,25 @@ def stratified_split(y, proportion=0.8):
 
 train, test = stratified_split(df['class'])
 
-X_train = df.iloc[train, 0:8]
-X_test = df.iloc[test, 0:8]
+def norm_arr(arr):
+    mean = arr.mean()
+    std = arr.std()
+
+    normalized = (arr - mean) / std
+    return normalized
+
+
+def norm_df(df):
+    result = df.copy()
+
+    for feature in df.columns:
+        result[feature] = norm_arr(result[feature])
+
+    return result
+
+
+X_train = norm_df(df.iloc[train, 0:8])
+X_test = norm_df(df.iloc[test, 0:8])
 
 y_train = df['class'][train]
 y_test = df['class'][test]
@@ -39,8 +57,6 @@ logreg.fit(X_train,y_train)
 
 def accuracy(y_test, y_pred):
     return 1 - sum(abs(y_test - y_pred)/len(y_test))
-
-
 
 y_pred=logreg.predict(X_test)
 
