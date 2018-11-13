48f756d9d82607de70eee114a9a7388f54b999b0
Kirill Shevchenko <kirills167@gmail.com>
2018-11-13 17:46:52 +0200

    stratified split

--- experiment-pipeline/main.py
+++ experiment-pipeline/main.py
@@ -1,9 +1,37 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
 
 df = pd.read_csv('indians-diabetes.csv', names=names)
 
-df.groupby('class').plas.hist(alpha=0.4)
+def stratified_split(y, proportion=0.8):
+    y = np.array(y)
 
-plt.show()
+    train_inds = np.zeros(len(y), dtype=bool)
+    test_inds = np.zeros(len(y), dtype=bool)
+
+    values = np.unique(y)
+    for value in values:
+        value_inds = np.nonzero(y == value)[0]
+        np.random.shuffle(value_inds)
+
+        n = int(proportion * len(value_inds))
+
+        train_inds[value_inds[:n]] = True
+        test_inds[value_inds[n:]] = True
+
+    return train_inds, test_inds
+
+train, test = stratified_split(df)
+
+X_train = df.iloc[train, 0:8]
+X_test = df.iloc[test, 0:8]
+
+y_train = df['class'][train]
+y_test = df['class'][test]
+
+#plt.show()
+
+#print(X_train)
+
